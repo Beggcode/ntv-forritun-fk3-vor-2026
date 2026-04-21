fe31b6cb6886ff69264c7c372413995137f6bcaf
Beggcode <Beggman95@gmail.com>
2026-04-21 19:35:32 +0000

    mah test

--- lesson13/src/lib/math.test.ts
+++ lesson13/src/lib/math.test.ts
@@ -1,1 +1,9 @@
-describe('add', () => {});
+import { describe, it, expect } from "vitest";
+import { add } from "./math";
+
+describe("add", () => {
+	it("Leggur saman tvær tölur", () => {
+		const result = add(1, 1);
+		expect(result).toBe(2);
+	});
+});
