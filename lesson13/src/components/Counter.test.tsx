cb5255d0d785a1fd73c9a9ec17ab2fe26828df08
Beggcode <Beggman95@gmail.com>
2026-04-21 19:12:28 +0000

    Counter test

--- lesson13/src/components/Counter.test.tsx
+++ lesson13/src/components/Counter.test.tsx
@@ -1,1 +1,15 @@
-describe('Counter', () => {});
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { describe, it, expect } from "vitest";
+import { Counter } from "./Counter";
+
+describe("Counter", () => {
+	it("Hækkar counter", async () => {
+		const user = userEvent.setup();
+		render(<Counter />);
+
+		await user.click(screen.getByRole("button", { name: "Hækka" }));
+
+		expect(screen.getByText("1")).toBeInTheDocument();
+	});
+});
