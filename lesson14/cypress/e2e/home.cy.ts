be378cdd1cd653f8970a5bc0f22ce43d0bb57c6b
Beggcode <Beggman95@gmail.com>
2026-05-05 17:13:34 +0000

    login e2e test and cypruss config

--- lesson14/cypress/e2e/home.cy.ts
+++ lesson14/cypress/e2e/home.cy.ts
@@ -1,1 +1,17 @@
-describe('Heimasíða (E2E)', () => {});
+describe("Homepage (E2E)", () => {
+	it("should navigate to the login page", () => {
+		cy.visit("/");
+		cy.contains("Skrá inn").click();
+		cy.url().should("include", "/login");
+	});
+
+	it("should show the dashboard after a successful login", () => {
+		cy.visit("/login");
+
+		cy.get("input").first().type("apa@rass.is");
+		cy.get("input").last().type("password12345{enter}");
+
+		cy.url().should("include", "/velkominn");
+		cy.contains("Velkomin").should("be.visible");
+	});
+});
